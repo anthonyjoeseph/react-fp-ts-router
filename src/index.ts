2169e931435a91ab652df0267bcc0ecb00250951
anthonygabriele <anthonyjoeseph@gmail.com>
2020-03-29 21:44:06 -0400

    export more types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import withCallbackRoutes from "./withCallbackRoutes";
+import withCallbackRoutes, {
+  AppStateWithRoute,
+  UpdateState,
+  NewStateFromRoute,
+} from "./withCallbackRoutes";
 import withNarrowerAppState from "./withNarrowerAppState";
 
 export {
   withCallbackRoutes,
   withNarrowerAppState,
+  AppStateWithRoute,
+  UpdateState,
+  NewStateFromRoute,
 };
