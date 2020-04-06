b87dbcf1a067740539208d24b9fca73ba546cff6
anthonygabriele <anthonyjoeseph@gmail.com>
2020-04-06 17:53:13 -0400

    update gitignore

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import withRouter, { DefaultStateFromRoute, UpdateState, StateWithRequest, Router, RouterResponse } from "./withRouter";
-import withNarrowerAppState from "./withNarrowerAppState";
-export default withRouter;
-export { withNarrowerAppState, UpdateState, StateWithRequest, DefaultStateFromRoute, Router, RouterResponse, };
